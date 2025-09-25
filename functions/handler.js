ce3e902976c0fc1780c1bbf335b3cb92b080144a
JJH <jeww1234@gmail.com>
2025-09-26 01:59:53 +0900

    test

--- functions/handler.js
+++ functions/handler.js
@@ -2,7 +2,7 @@
 import fetch from "node-fetch";
 
 export async function handler(event) {
-  const keyword = event.queryStringParameters.keyword; // keyword로 받아야 함
+  const keyword = event.queryStringParameters.keyword;
   const API_KEY = process.env.API_KEY;
 
   const url = `https://www.nl.go.kr/NL/search/openApi/search.do?key=${API_KEY}&srchTarget=total&kwd=${encodeURIComponent(keyword)}&pageNum=1&pageSize=10&category=도서&apiType=json`;
@@ -10,9 +10,13 @@ export async function handler(event) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log("ddd", data)
-    return { statusCode: 200, body: JSON.stringify(data) };
+    console.log("API 결과:", data);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
   } catch (err) {
+    console.error("API 호출 실패", err);
     return { statusCode: 500, body: JSON.stringify({ error: "API 호출 실패" }) };
   }
 }
